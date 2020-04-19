29bd1b10139eb18a97e81589bffc4d30decc4eca
shubham shahare <shubham.shahare@agrostar.in>
2020-04-19 13:30:30 +0530

    Added examples of iterator and generator

--- pythoncookbook/section1/priorityqueue.py
+++ pythoncookbook/section1/priorityqueue.py
@@ -5,13 +5,16 @@ class PriorityQueue():
         self._queue = []
         self._index = 0
     def append(self,ele, priority):
-        heapq.heappush(self._queue,(ele,priority, self._index))
+        heapq.heappush(self._queue,(-priority, self._index,ele))
         self._index +=1
     def pop(self):
-        return heapq.heappop(self._queue)
+        return heapq.heappop(self._queue)[-1]
 
 
 pq = PriorityQueue()
 pq.append('a',3)
+pq.append('bob',1)
+pq.append('john',4)
+print(pq.pop())
 print(pq.pop())
 
